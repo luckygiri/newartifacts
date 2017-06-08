6f8e8db98e4eea7d12bfe4d1ad275d5e36f91c7a
luckygiri <gp2713@gmail.com>
2017-06-08 15:55:53 +0530

    Create startinstall.ps1

--- custom/VSAndroidEmulator/startinstall.ps1
+++ custom/VSAndroidEmulator/startinstall.ps1
@@ -57,19 +57,19 @@ trap
 
 try
 {
-    $NewDIR = "C:\SoftwaresDump\QTP12.5"
-    $SoftwareWebLink = "https://download.microsoft.com/download/e/4/c/e4c393a9-8fff-441b-ad3a-3f4040317a1f/vs_community.exe"
-    $SoftwarePath = "C:\SoftwaresDump\QTP12.5\vs_community.exe"
+    $NewDIR = "C:\SoftwaresDump"
+    $SoftwareWebLink = "https://download.microsoft.com/download/3/E/6/3E6568E8-D444-4B1B-9BA2-817DEA2196D6/20160622.2/vs_emulatorsetup.exe"
+    $SoftwarePath = "C:\SoftwaresDump\vs_emulatorsetup.exe"
 
     Write-Output 'Preparing temp directory ...'
-    New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -Force | Out-Null
+    New-Item "C:\SoftwaresDump" -ItemType Directory -Force | Out-Null
 
     Write-Output 'Downloading pre-requisite files ...'
     (New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
    
 
  Write-Output 'Installing ...'
-Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe" -ArgumentList '/q' -Wait 
+Start-Process "C:\SoftwaresDump\vs_emulatorsetup.exe" -ArgumentList '/q' -Wait 
 
     
 
