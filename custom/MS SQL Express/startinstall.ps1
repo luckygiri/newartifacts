889703b6fdbc03f3dd7e9136353e78cd038ba388
luckygiri <gp2713@gmail.com>
2017-06-08 17:32:13 +0530

    Create startinstall.ps1

--- custom/MS SQL Express/startinstall.ps1
+++ custom/MS SQL Express/startinstall.ps1
@@ -57,19 +57,19 @@ trap
 
 try
 {
-    $NewDIR = "C:\SoftwaresDump\QTP12.5"
-    $SoftwareWebLink = "https://download.microsoft.com/download/e/4/c/e4c393a9-8fff-441b-ad3a-3f4040317a1f/vs_community.exe"
-    $SoftwarePath = "C:\SoftwaresDump\QTP12.5\vs_community.exe"
+    $NewDIR = "C:\SoftwaresDump"
+    $SoftwareWebLink = "http://download.microsoft.com/download/B/F/2/BF2EDBB8-004D-47F3-AA2B-FEA897591599/SQLServer2016-SSEI-Expr.exe"
+    $SoftwarePath = "C:\SoftwaresDump\SQLServer2016-SSEI-Expr.exe"
 
     Write-Output 'Preparing temp directory ...'
-    New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -Force | Out-Null
+    New-Item "C:\SoftwaresDump" -ItemType Directory -Force | Out-Null
 
     Write-Output 'Downloading pre-requisite files ...'
     (New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
    
 
  Write-Output 'Installing ...'
-Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe" -ArgumentList '/q' -Wait 
+Start-Process "C:\SoftwaresDump\SQLServer2016-SSEI-Expr.exe" -ArgumentList '/q' -Wait 
 
     
 
